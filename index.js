dc7b7b92819c294da2f69663ae40f7e79efe6212
David Stinnette <dastinnette@gmail.com>
2023-08-24 19:44:24 -0400

    Completed intro to functions

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Follow along with the examples here
+
+function sayHello() {
+    console.log("Hello!")
+}
+
+sayHello()
+
+function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+}
+
+say("Julio", "hello")
+  
+function add(x, y) {
+    return x + y
+}
+
+console.log(add(1, 2))
